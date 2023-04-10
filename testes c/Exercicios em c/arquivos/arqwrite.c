6e6cfc1e375c498ba301defc79a01b7751e0bd5b
TigasMoro <122630395+TigasMoro@users.noreply.github.com>
2023-04-10 15:52:56 -0300

    Update arqwrite.c

--- testes c/Exercicios em c/arquivos/arqwrite.c
+++ testes c/Exercicios em c/arquivos/arqwrite.c
@@ -1,10 +1,12 @@
+//Escreve em um arquivo txt
+
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(void){
     FILE *file;
     file = fopen("C:\\Users\\roger\\Desktop\\curso programacao\\LInguagem C\\Exercicios em c\\arquivos\\arq.txt", "w");
-    fprintf(file, "Tiago de Souza Moro Conke");
+    fprintf(file, "Tiago Conke");
     fclose(file);
 
  return 0;
